f5fe01f12255780515b57b79bed90ccc14794c53
evgeniymin <zuzuka7777777@gmail.com>
2023-07-14 21:52:48 +0300

    Made game.js into a reactjs script

--- my-app/src/Game.js
+++ my-app/src/Game.js
@@ -1,59 +1,97 @@
+import React, { useState } from 'react';
+import './Game.css';
 
-game = {
-  Xhod: true,
-  Xh: [],
-  Oh: [],
-  win: [
-      ['2', '5', '8'],
-      ['0', '4', '8'],
-      ['2', '4', '6'],
-      ['0', '1', '2'],
-      ['3', '4', '5'],
-      ['6', '7', '8'],
-      ['0', '3', '6'],
-      ['1', '4', '7'],
-  ]
-}
+const Game = () => {
+  const [xhod, setXhod] = useState(true);
+  const [xh, setXh] = useState([]);
+  const [oh, setOh] = useState([]);
+  const [winner, setWinner] = useState(null);
 
-document.addEventListener('click', event => {
-  const cel = event.target
-  const iCell = cel.classList.contains('grid')
-  const Disabled = cel.classList.contains('disabled')
+  const win = [
+    ['2', '5', '8'],
+    ['0', '4', '8'],
+    ['2', '4', '6'],
+    ['0', '1', '2'],
+    ['3', '4', '5'],
+    ['6', '7', '8'],
+    ['0', '3', '6'],
+    ['1', '4', '7'],
+  ];
 
-  if (iCell && !Disabled) {
-      const cellValue = cel.dataset.value
+  const handleClick = (event) => {
+    const cell = event.target;
+    const iCell = cell.classList.contains('grid');
+    const disabled = cell.classList.contains('disabled');
 
-      game.xhod === true
-          ? game.Xh.push(cellValue)
-          : game.Oh.push(cellValue)
+    if (iCell && !disabled && !winner) {
+      const cellValue = cell.dataset.value;
 
-      cel.classList.add('disabled')
-      cel.classList.add(game.xhod ? 'x' : 'o')
+      if (xhod) {
+        setXh([...xh, cellValue]);
+      } else {
+        setOh([...oh, cellValue]);
+      }
 
-      game.xhod = !game.xhod
+      cell.classList.add('disabled');
+      cell.classList.add(xhod ? 'x' : 'o');
 
-      game.win.forEach(win => {
-          const xWins = win.every(state => game.Xh.includes(state))
-          const oWins = win.every(state => game.Oh.includes(state))
+      setXhod(!xhod);
+    }
+  };
 
-          if (xWins) {
-              document.querySelectorAll('.grid').forEach(cell => cell.classList.add('disabled'))
-              alert('Х Победили!')
-          }
-          if (oWins) {
-              document.querySelectorAll('.grid').forEach(cell => cell.classList.add('disabled'))
-              alert('О Победили!')
-          }
-      })
-  }
-})
+  const checkWin = (player) => {
+    for (let i = 0; i < win.length; i++) {
+      const [a, b, c] = win[i];
+      if (player.includes(a) && player.includes(b) && player.includes(c)) {
+        return true;
+      }
+    }
+    return false;
+  };
 
-document.querySelector('.restart').addEventListener('click', () => {
-  document.querySelectorAll('.grid').forEach(cell => {
-      cell.classList.remove('disabled', 'x', 'o')
-  })
+  const handleRestart = () => {
+    document.querySelectorAll('.grid').forEach((cell) => {
+      cell.classList.remove('disabled', 'x', 'o');
+    });
 
-  game.xhod = true
-  game.Xh = []
-  game.Oh = []
-})
+    setXhod(true);
+    setXh([]);
+    setOh([]);
+    setWinner(null);
+  };
+  
+  React.useEffect(() => {
+    if (checkWin(xh)) {
+      setWinner('X');
+    } else if (checkWin(oh)) {
+      setWinner('O');
+    }
+  }, [xh, oh]);
+
+  return (
+    <div>
+      <div id="game">
+        <div className="grid" data-value="0" onClick={handleClick}></div>
+        <div className="grid" data-value="1" onClick={handleClick}></div>
+        <div className="grid" data-value="2" onClick={handleClick}></div>
+        <div className="grid" data-value="3" onClick={handleClick}></div>
+        <div className="grid" data-value="4" onClick={handleClick}></div>
+        <div className="grid" data-value="5" onClick={handleClick}></div>
+        <div className="grid" data-value="6" onClick={handleClick}></div>
+        <div className="grid" data-value="7" onClick={handleClick}></div>
+        <div className="grid" data-value="8" onClick={handleClick}></div>
+      </div>
+
+      {winner && (
+        <div className="message">
+          {winner === 'X' ? 'X Wins!' : 'O Wins!'}
+          <button className="restart" onClick={handleRestart}>
+            Restart
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Game;
