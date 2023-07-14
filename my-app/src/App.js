03ae0a9fdfed86a722e63074d2832e9ccc1f0424
evgeniymin <zuzuka7777777@gmail.com>
2023-07-14 20:45:42 +0300

    Took everything to myapp folder

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Game from './Game';
+import './App.css';
+
+
+function App() {
+  return (
+    <div>
+      <h1>Крестики-Нолики
+      </h1>
+      <Game />
+    </div>
+  );
+}
+
+export default App;
